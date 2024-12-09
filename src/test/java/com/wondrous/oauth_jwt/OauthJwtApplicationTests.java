e847d99085c10bbcd2d4464e54cae1e07d568d89
KKT <kimgt2828@gmail.com>
2024-12-09 11:35:21 +0900

    setting: 프로젝트 기본 설정

---
+++ src/test/java/com/wondrous/oauth_jwt/OauthJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wondrous.oauth_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
